11f9fd6dd0992ab8a641033bf6f67c86e9781305
Aleksey Lovchikov <svdmusic@gmail.com>
2023-08-20 18:02:42 +0300

    add api

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -6,40 +6,105 @@ import { Table } from '../shared/ui/Table';
 export const _links = [
   {
     id: 1,
-    title: 'Hello',
-    release_date: '1986-03-01',
-    runtime: 116,
-    mpaa_rating: 'R',
+    title: 'Gym',
     description: 'Some long description',
+    phone: '8 999 123 12 12',
+    email: 'test@mail.com',
+    category: 'sport',
+    links: 'https://gym.com',
+    added_at: '2023-03-01',
   },
   {
     id: 2,
-    title: 'World',
-    release_date: '1996-03-01',
-    runtime: 115,
-    mpaa_rating: 'PG-13',
+    title: 'Bean bar',
     description: 'Some long description',
+    phone: '8 999 123 12 12',
+    email: 'test@mail.com',
+    category: 'coffee',
+    links: 'https://google.com',
+    added_at: '2021-10-12',
   },
   {
     id: 112,
     title: 'Невролог - Лимассол',
-    release_date: '2023-03-01',
-    runtime: 115,
-    mpaa_rating: 'PG-13',
     description: 'Тут могут быть контакты',
+    phone: '8 999 123 12 12',
+    email: 'test@mail.com',
+    category: 'doctor',
+    links: 'https://google.com',
+    added_at: '2023-03-01',
+  },
+  {
+    id: 412312,
+    title: 'Невролог - Лимассол',
+    description: 'Тут могут быть контакты',
+    phone: '8 999 123 12 12',
+    email: 'test@mail.com',
+    category: 'doctor',
+    links: 'https://google.com',
+    added_at: '2023-03-01',
+  },
+  {
+    id: 412121,
+    title: 'Невролог - Лимассол',
+    description: 'Тут могут быть контакты',
+    phone: '8 999 123 12 12',
+    email: 'test@mail.com',
+    category: 'doctor',
+    links: 'https://google.com',
+    added_at: '2023-03-01',
+  },
+  {
+    id: 11221,
+    title: 'Невролог - Лимассол',
+    description: 'Тут могут быть контакты',
+    phone: '8 999 123 12 12',
+    email: 'test@mail.com',
+    category: 'doctor',
+    links: 'https://google.com',
+    added_at: '2023-03-01',
   },
 ];
+
+const temp = (
+  <>
+    <h1 className="mb-1" style={{ color: 'tomato' }}>
+      *** <u>HERE WILL BE A SEARCH</u> ***
+    </h1>
+    <h1 className="mb-1" style={{ color: 'tomato' }}>
+      *** <u>HERE WILL BE A BADGE WITH CATEGORIES</u> ***
+    </h1>
+  </>
+);
 
 const Links = () => {
   const [links, setLinks] = useState([]);
 
   useEffect(() => {
-    setLinks(_links);
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    const reqOptions = {
+      method: 'GET',
+      headers,
+    };
+
+    fetch(`http://localhost:8080/links`, reqOptions)
+      .then((res) => res.json())
+      .then((data) => {
+        setLinks(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   return (
     <div>
       <Title>Links</Title>
+
+      {temp}
+
       <Table links={links} />
     </div>
   );
--- src/shared/ui/Table/Table.jsx
+++ src/shared/ui/Table/Table.jsx
@@ -1,28 +1,34 @@
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export const Table = ({ links }) => {
-    return (
-        <table>
-            <thead>
-            <tr>
-                <th>Title</th>
-                <th>Created at</th>
-                <th>Rating</th>
-            </tr>
-            </thead>
-            <tbody>
-            {links.map(link => (
-                <tr key={link.id}>
-                    <td>
-                        <Link to={`/links/${link.id}`}>
-                            {link.title}
-                        </Link>
-                    </td>
-                    <td>{link.release_date}</td>
-                    <td>{link.mpaa_rating}</td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
-    );
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Phone</th>
+          <th>Email</th>
+          <th>Category</th>
+          <th>Links</th>
+          <th>Created at</th>
+        </tr>
+      </thead>
+      <tbody>
+        {links.map((link) => (
+          <tr key={link.id}>
+            <td>
+              <Link to={`/links/${link.id}`}>{link.title}</Link>
+            </td>
+            <td>{link.description}</td>
+            <td>{link.phone}</td>
+            <td>{link.email}</td>
+            <td>{link.category}</td>
+            <td>{link.links}</td>
+            <td>{link.added_at}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
